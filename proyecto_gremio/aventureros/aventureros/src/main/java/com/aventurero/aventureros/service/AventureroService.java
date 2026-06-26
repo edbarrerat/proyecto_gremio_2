3cb4921fa5544268184a45424dcd85bde099b155
Eduardo Barrera <edbarrerat@duocuc.cl>
2026-06-26 15:12:30 -0400

    pequeños cambios al servicio

--- proyecto_gremio/aventureros/aventureros/src/main/java/com/aventurero/aventureros/service/AventureroService.java
+++ proyecto_gremio/aventureros/aventureros/src/main/java/com/aventurero/aventureros/service/AventureroService.java
@@ -42,26 +42,23 @@ public class AventureroService {
     }
 
     public String eliminar(Integer id){
-        try {
-            Aventurero aventurero = aventureroRepository.findById(id)
+        Aventurero aventurero = aventureroRepository.findById(id)
             .orElseThrow(()->new RuntimeException("No se puede eliminar: el aventurero con Id"+id+" no está registrado."));
-            aventureroRepository.delete(aventurero);
-            return "El aventurero '"+aventurero.getNombre()+"' ha sido eliminado exitosamente de los registros.";
-        } catch (RuntimeException e) {
-            return e.getMessage();
+        aventureroRepository.delete(aventurero);
+        return "El aventurero '"+aventurero.getNombre()+"' ha sido eliminado exitosamente de los registros.";
         }
-    }
+    
 
-    public Aventurero guardarAventurero(Aventurero aventurero){
-        return aventureroRepository.save(aventurero);
+    public AventureroDTO guardarAventurero(Aventurero aventurero){
+        return convertirADTO(aventurero);
     }
     
-    public Aventurero actualizarAventurero(Integer id,Aventurero aventurero){
+    public AventureroDTO actualizarAventurero(Integer id,Aventurero aventurero){
         Aventurero aven = aventureroRepository.findById(id).orElseThrow(() -> new RuntimeException("El aventurero no está en los registros."));
         if(aventurero.getNombre() != null){
             aven.setNombre(aventurero.getNombre());
         }
-        return aventureroRepository.save(aven);
+        return convertirADTO(aven);
     }
 
     public AventureroDTO asignarProfesion(Integer aventureroId, Integer profesionId) {
